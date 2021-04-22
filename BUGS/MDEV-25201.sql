d4ca27df9f335f2c896056560f244f4e6e84491c
mariadb-RameshSivaraman <ramesh.sivaraman@mariadb.com>
2021-04-22 23:41:01 +0530

    Bug updates

--- BUGS/MDEV-25201.sql
+++ BUGS/MDEV-25201.sql
@@ -2,3 +2,8 @@ SET GLOBAL wsrep_replicate_myisam= ON;
 CREATE TEMPORARY TABLE t1 (i INT, PRIMARY KEY pk (i)) ENGINE=MyISAM;
 PREPARE stmt FROM "INSERT INTO t1 (id) SELECT * FROM (SELECT 4 AS i) AS y";
 INSERT INTO t1 VALUES(4);
+
+CREATE TABLE t (a INT KEY);
+SET GLOBAL wsrep_replicate_myisam=ON;
+PREPARE stmt FROM 'UPDATE mysql.user SET authentication_string=(?) WHERE USER=?';
+INSERT INTO t VALUES (0xA7C3);
