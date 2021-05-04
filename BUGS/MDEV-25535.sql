580b96f8a1f681a4eeab5fb78bd70ddb8407a4d4
mariadb-RameshSivaraman <ramesh.sivaraman@mariadb.com>
2021-05-05 00:20:25 +0530

    Bug updates

--- BUGS/MDEV-25535.sql
+++ BUGS/MDEV-25535.sql
@@ -8,3 +8,11 @@ SET SESSION wsrep_trx_fragment_size=1;
 INSERT INTO t2 VALUES (10);
 SAVEPOINT event_logging_1;
 CREATE TABLE IF NOT EXISTS t3 (id INT) ENGINE=InnoDB;
+
+CREATE TABLE t1 AS SELECT 1 AS c1;
+SET GLOBAL wsrep_ignore_apply_errors=0;
+SET SESSION wsrep_trx_fragment_size=1;
+SET SESSION wsrep_trx_fragment_unit='statements';
+SET AUTOCOMMIT=0;
+CREATE TABLE t1 (id INT);
+SAVEPOINT SVP001;
