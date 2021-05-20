ccb59ff601dc7276125d6c57662e875a24a836eb
Roel Van de Paar <roel.vandepaar@mariadb.com>
2021-05-20 10:37:19 +1000

    Adding ~/scanerr (added) in favor of /data/scan_errorlogs (removed), bug add

---
+++ BUGS/MDEV-25753.sql
@@ -0,0 +1,8 @@
+# Repeat until server crashes (about 20 to 1000 attempts; quantity may vary signficantly)
+SET MAX_STATEMENT_TIME=0.000001;
+HELP 'a%';
+HELP 'a%';
+HELP 'a%';
+HELP 'a%';
+HELP 'a%';
+HELP 'a%';
