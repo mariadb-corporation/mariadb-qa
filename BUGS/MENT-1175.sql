271b000479d14006746196e3de2bcaef5590cc10
mariadb-RameshSivaraman <ramesh.sivaraman@mariadb.com>
2021-08-04 15:48:40 +0530

    Bug updates

--- BUGS/MENT-1175.sql
+++ BUGS/MENT-1175.sql
@@ -2,3 +2,16 @@ XA START 'a';
 XA END 'a';
 CACHE INDEX t1,t2 in default;
 XA PREPARE 'a';
+
+SET GLOBAL wsrep_mode=REPLICATE_ARIA;
+XA START 'a';
+DELETE FROM sys.sys_config WHERE variable = 'statement_performance_analyzer.view';
+XA END 'a';
+XA PREPARE 'a';
+
+SET GLOBAL wsrep_mode=REPLICATE_ARIA;
+XA START 'a';
+DELETE FROM sys.sys_config WHERE variable = 'statement_performance_analyzer.view';
+XA END 'a';
+XA PREPARE 'a';
+COMMIT;
