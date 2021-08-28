29dc8dba4dca71f8f1458e6865bdd7ef61c16179
Roel Van de Paar <roel.vandepaar@mariadb.com>
2021-08-28 14:03:20 +1000

    pca improvement

--- pquery-clean-all.sh
+++ pquery-clean-all.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 SCRIPT_PWD=$(cd "`dirname $0`" && pwd)  # Script location for pquery-clean-known.sh, which is in the same directory
-ls -d --color=never [0-9][0-9][0-9][0-9][0-9][0-9] | xargs -I{} echo "if [ -d ./{} ]; then cd {}; ${SCRIPT_PWD}/pquery-clean-known.sh ${1}; cd - >/dev/null; fi" | xargs -P10 -I{} bash -c "{}"
+ls -d --color=never [0-9][0-9][0-9][0-9][0-9][0-9] | xargs -I{} echo "if [ -d ./{} ]; then cd {}; echo -n '{}: '; ${SCRIPT_PWD}/pquery-clean-known.sh ${1}; cd - >/dev/null; else echo '{}: Failed?'; fi" | xargs -P1 -I{} bash -c "{}"
