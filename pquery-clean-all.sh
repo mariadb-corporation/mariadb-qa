8f04779bfa35895caafc662dade7ff036b47534f
Roel Van de Paar <roel.vandepaar@mariadb.com>
2021-09-06 07:44:50 +1000

    Bug updates, and reverted pquery-clean-all.sh to multithreaded to increase speed of cleanups

--- pquery-clean-all.sh
+++ pquery-clean-all.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 SCRIPT_PWD=$(cd "`dirname $0`" && pwd)  # Script location for pquery-clean-known.sh, which is in the same directory
-ls -d --color=never [0-9][0-9][0-9][0-9][0-9][0-9] | xargs -I{} echo "if [ -d ./{} ]; then cd {}; echo -n '{}: '; ${SCRIPT_PWD}/pquery-clean-known.sh ${1}; cd - >/dev/null; else echo '{}: Failed?'; fi" | xargs -P1 -I{} bash -c "{}"
+# Note: the -P50 renders the '{}' subdirs (numbers) output hard to read (as they are output first by this script before starting pquery-clean-known.sh for each of those subdirs, but it is much faster. If/when debugging is needed, just set it to -P1 then revert to -Px afterwards.
+ls -d --color=never [0-9][0-9][0-9][0-9][0-9][0-9] | xargs -I{} echo "if [ -d ./{} ]; then cd {}; echo -n '{}: '; ${SCRIPT_PWD}/pquery-clean-known.sh ${1}; cd - >/dev/null; else echo '{}: Failed?'; fi" | xargs -P50 -I{} bash -c "{}"
