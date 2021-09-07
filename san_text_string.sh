c075bccec0ad6b47d5440f41417a0943394578ca
Roel Van de Paar <roel.vandepaar@mariadb.com>
2021-09-07 21:00:53 +1000

    san_text_string.sh implemented full UNIT testing and finished code fixes for UBSAN

--- san_text_string.sh
+++ san_text_string.sh
@@ -177,22 +177,22 @@ while IFS=$'\n' read LINE; do
       flag_ready_check
       FLAG_ASAN_IN_PROGRESS=0; FLAG_TSAN_IN_PROGRESS=0; FLAG_UBSAN_IN_PROGRESS=1
       UBSAN_FRAME1=; UBSAN_FRAME2=; UBSAN_FRAME3=; UBSAN_FRAME4=
-      UBSAN_ERROR="$(echo "${LINE}" | sed 's|.*runtime error:|runtime error:|')"
+      UBSAN_ERROR="$(echo "${LINE}" | sed 's|.*runtime error:|runtime error:|;s|load of value [0-9]\+|load of value X|g;s|applying non-zero offset [0-9]\+|applying non-zero offset X|g;s|shift exponent [0-9]\+|shift exponent X|g;s|index [0-9]\+ out of bounds|index X out of bounds|g')"
       UBSAN_FILE_PREPARSE="$(echo "${LINE}" | sed 's| runtime error:.*||;s|:[0-9]\+:[0-9]\+:[ ]*$||;s|.*/client/|client/|;s|.*/cmake/|cmake/|;s|.*/dbug/|dbug/|;s|.*/debian/|debian/|;s|.*/extra/|extra/|;s|.*/include/|include/|;s|.*/libmariadb/|libmariadb/|;s|.*/libmysqld/|libmysqld/|;s|.*/libservices/|libservices/|;s|.*/mysql-test/|mysql-test/|;s|.*/mysys/|mysys/|;s|.*/mysys_ssl/|mysys_ssl/|;s|.*/plugin/|plugin/|;s|.*/scripts/|scripts/|;s|.*/sql/|sql/|;s|.*/sql-bench/|sql-bench/|;s|.*/sql-common/|sql-common/|;s|.*/storage/|storage/|;s|.*/strings/|strings/|;s|.*/support-files/|support-files/|;s|.*/tests/|tests/|;s|.*/tpool/|tpool/|;s|.*/unittest/|unittest/|;s|.*/vio/|vio/|;s|.*/win/|win/|;s|.*/wsrep-lib/|wsrep-lib/|;s|.*/zlib/|zlib/|;s|.*/components/|components/|;s|.*/libbinlogevents/|libbinlogevents/|;s|.*/libbinlogstandalone/|libbinlogstandalone/|;s|.*/libmysql/|libmysql/|;s|.*/router/|router/|;s|.*/share/|share/|;s|.*/testclients/|testclients/|;s|.*/utilities/|utilities/|;s|.*/regex/|regex/|;')"  # Drop path prefix (build directory), leaving only relevant part for MD/MS
     fi
     if [ "${FLAG_UBSAN_IN_PROGRESS}" -eq 1 ]; then
       # Parse first 4 stack frames if discovered in current line
       if [[ "${LINE}" == *"#0 0x"* ]]; then
-        UBSAN_FRAME1="$(echo "${LINE}" | sed 's|^[^i]\+in[ ]\+||;s|[ ]\+.*||;s|(.*)[ ]*$||')"
+        UBSAN_FRAME1="$(echo "${LINE}" | sed 's|^[^i]\+in[ ]\+||;s|(.*)||g;s|[ ]\+.*||')"
       fi
       if [[ "${LINE}" == *" #1 0x"* ]]; then
-        UBSAN_FRAME2="$(echo "${LINE}" | sed 's|^[^i]\+in[ ]\+||;s|[ ]\+.*||;s|(.*)[ ]*$||')"
+        UBSAN_FRAME2="$(echo "${LINE}" | sed 's|^[^i]\+in[ ]\+||;s|(.*)||g;s|[ ]\+.*||')"
       fi
       if [[ "${LINE}" == *" #2 0x"* ]]; then
-        UBSAN_FRAME3="$(echo "${LINE}" | sed 's|^[^i]\+in[ ]\+||;s|[ ]\+.*||;s|(.*)[ ]*$||')"
+        UBSAN_FRAME3="$(echo "${LINE}" | sed 's|^[^i]\+in[ ]\+||;s|(.*)||g;s|[ ]\+.*||')"
       fi
       if [[ "${LINE}" == *" #3 0x"* ]]; then
-        UBSAN_FRAME4="$(echo "${LINE}" | sed 's|^[^i]\+in[ ]\+||;s|[ ]\+.*||;s|(.*)[ ]*$||')"
+        UBSAN_FRAME4="$(echo "${LINE}" | sed 's|^[^i]\+in[ ]\+||;s|(.*)||g;s|[ ]\+.*||')"
         FLAG_UBSAN_IN_PROGRESS=0  # UBSAN issues are herewith fully defined
         FLAG_UBSAN_READY=1
       fi
