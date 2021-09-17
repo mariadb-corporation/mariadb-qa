5c508085c50098c03ccb249469fedcb7ccd03ac7
Roel Van de Paar <roel.vandepaar@mariadb.com>
2021-09-18 07:25:01 +1000

    Minor build updates

--- mariadb-build-qa/buildall_opt.sh
+++ mariadb-build-qa/buildall_opt.sh
@@ -10,7 +10,7 @@ else
 fi
 
 DIR=${PWD}
-rm -Rf 10.1_opt 10.2_opt 10.3_opt 10.4_opt 10.5_opt 10.6_opt
+rm -Rf 10.1_opt 10.2_opt 10.3_opt 10.4_opt 10.5_opt 10.6_opt 10.7_opt
 #cd ${DIR}/10.1 && ~/mariadb-qa/build_mdpsms_opt.sh &
 cd ${DIR}/10.2 && ~/mariadb-qa/build_mdpsms_opt.sh &
 cd ${DIR}/10.3 && ~/mariadb-qa/build_mdpsms_opt.sh &
