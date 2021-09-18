db26a3cb8424ba136ee3616928970dbdd23333bb
Roel Van de Paar <roel.vandepaar@mariadb.com>
2021-09-18 11:27:03 +1000

    find_cause final #3/3

--- find_cause.sh
+++ find_cause.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+# Note: do not add end of line filtering of ";# ..." as that may often contain clues about lines just before/after which caused the issue
+
 if [ -z "${1}" ]; then
   echo "This script finds likely causes in the provided SQL input file for user drops, access denied, etc."
   echo "Please specify an SQL input file"
   exit 1
 fi
 
-grep --binary-files=text -n -iE 'RELEASE|KILL|USER|ROOT|SHUTDOWN|PRIVILEGES|_PRIV' "${1}"
+echo '====== All possibilties'
+grep --binary-files=text -n -iE 'RELEASE|KILL|USER|REVOKE|ROOT|SHUTDOWN|PRIVILEGES|_PRIV|DROP.*host|DROP.*user' "${1}" 
+
+echo '====== Most likely possibilties (check lines just before these manually in "All possibilties" output above)'
+grep --binary-files=text -n -iE ' RELEASE|KILL |USER.*ROOT|REVOKE.*ROOT|SHUTDOWN|DROP.*mysql.*host|DROP.*mysql.*user' "${1}" 
+
+echo '====== See "All possibilties" and "Most likely possibilties" above. If all else fails, SOURCE the sql file at the command line and observe which line things start failing at'
