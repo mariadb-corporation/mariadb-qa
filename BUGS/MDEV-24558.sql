d49519d19c7e99209373bbb60dba8749e4929fa4
Roel Van de Paar <roel.vandepaar@mariadb.com>
2022-01-20 16:18:40 +1100

    Bug updates

--- BUGS/MDEV-24558.sql
+++ BUGS/MDEV-24558.sql
@@ -1,3 +1,7 @@
 CREATE TABLE t(c POINT NOT NULL) ENGINE=InnoDB;
 DROP TABLE mysql.innodb_table_stats;
 CREATE SPATIAL INDEX i ON t(c);
+
+CREATE TABLE t (c POINT PRIMARY KEY);
+DROP TABLE mysql.innodb_index_stats;
+ALTER TABLE t ADD SPATIAL INDEX i (c ASC);
