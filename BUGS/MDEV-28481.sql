a281221e228f83ab9761bd42aa2260211a49854c
Roel Van de Paar <roel.vandepaar@mariadb.com>
2022-05-21 11:46:56 +1000

    Bug updates

--- BUGS/MDEV-28481.sql
+++ BUGS/MDEV-28481.sql
@@ -3,3 +3,9 @@ SET SESSION collation_server=filename;
 CREATE DATABASE test;
 USE test;
 CREATE TABLE t (c CHAR BINARY);
+
+DROP DATABASE test;
+SET SESSION collation_server=filename;
+CREATE DATABASE test;
+USE test;
+CREATE TABLE t0 (c ENUM ('') BINARY);
