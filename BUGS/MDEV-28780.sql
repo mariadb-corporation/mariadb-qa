9127ab7f3a8d66fc221c8840a2c9470650b1cd06
mariadb-RameshSivaraman <ramesh.sivaraman@mariadb.com>
2022-06-10 12:34:52 +0530

    Bug updates

--- BUGS/MDEV-28780.sql
+++ BUGS/MDEV-28780.sql
@@ -3,3 +3,16 @@ ALTER TABLE mysql.column_stats RENAME TO mysql.column_stats1;
 CREATE TABLE t (a INT,b CHAR(1),c FLOAT);
 ALTER TABLE t RENAME mysql.column_stats;
 ALTER TABLE tab CHANGE COLUMN c3 c33 LINESTRING;
+
+CREATE TABLE t1 (f1 VARCHAR(1)) ENGINE=InnoDB;
+ALTER TABLE t1 ADD KEY2 INT, ADD KEY(KEY2);
+ALTER TABLE t1 RENAME t3;
+ALTER TABLE mysql.column_stats RENAME TO mysql.column_stats1;
+ALTER TABLE t3 RENAME mysql.column_stats;
+ALTER TABLE mysql.db DROP COLUMN delete_history_priv;
+
+ALTER TABLE mysql.column_stats RENAME TO mysql.column_stats0;
+CREATE TABLE t0 (a INT KEY);
+ALTER TABLE t0 RENAME mysql.column_stats;
+CREATE TABLE t (c INT,d INT);
+ALTER TABLE t DROP b,DROP c,DROP d,ADD COLUMN (b INT,c CHAR,d INT);
