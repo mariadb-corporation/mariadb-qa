74f32114c831ece95dc5841ff0e5df9df73ed8c4
Roel Van de Paar <roel.vandepaar@mariadb.com>
2022-08-23 16:49:19 +1000

    Bug updates, filter updates

--- BUGS/MDEV-28780.sql
+++ BUGS/MDEV-28780.sql
@@ -16,3 +16,9 @@ CREATE TABLE t0 (a INT KEY);
 ALTER TABLE t0 RENAME mysql.column_stats;
 CREATE TABLE t (c INT,d INT);
 ALTER TABLE t DROP b,DROP c,DROP d,ADD COLUMN (b INT,c CHAR,d INT);
+
+CREATE TABLE t (c FLOAT);
+ALTER TABLE mysql.column_stats RENAME TO mysql.column_stats1;
+CREATE TABLE t2 (a CHAR(20) BINARY);
+ALTER TABLE t RENAME mysql.column_stats;
+ALTER TABLE t2 CHANGE COLUMN a a CHAR(43);
