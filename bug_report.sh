49b40f1f64602295b914eda2ab2fe2ffb06383f7
Roel Van de Paar <roel.vandepaar@mariadb.com>
2022-09-28 15:04:52 +1000

    Small bugfix

--- bug_report.sh
+++ bug_report.sh
@@ -14,7 +14,6 @@ fi
 # Script variables: do not change
 SAN_BUILD_FOR_NON_SAN_REPORTS_OPT="/test/$(cd /test; ./gendirs.sh san | grep '10.1[0-9]' | grep 'opt' | sort -h | tail -n1)"
 SAN_BUILD_FOR_NON_SAN_REPORTS_DBG="$(echo "${SAN_BUILD_FOR_NON_SAN_REPORTS_OPT}" | sed 's|\-opt|-dbg|')"
-SAN_BUILD_FOR_NON_SAN_REPORTS_OPT=dummy
 
 if [ "${ALSO_TEST_SAN_BUILD_FOR_NON_SAN_REPORTS}" -eq 1 ]; then
   if [ "${1}" != "SAN" ]; then
