c1bed3055d108705277d3ede8eedb3275d94f70a
Roel Van de Paar <roel.vandepaar@mariadb.com>
2023-05-09 15:57:49 +1000

    Bug updates, script updates

--- BUGS/MDEV-24814.sql
+++ BUGS/MDEV-24814.sql
@@ -3,3 +3,8 @@ RENAME TABLE mysql.tables_priv TO mysql.tables_priv_bak;
 CREATE TABLE t (c INT) ENGINE=InnoDB;
 CREATE TABLE mysql.tables_priv SELECT * FROM mysql.tables_priv_bak;
 GRANT SELECT ON t TO m@localhost;
+
+SET sql_mode='';
+CREATE TABLE t (c INT);
+ALTER TABLE mysql.tables_priv DROP COLUMN TIMESTAMP;
+GRANT SELECT ON t TO u@localhost;
