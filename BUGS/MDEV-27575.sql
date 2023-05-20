98c03158776b93d7d8bcb8a59beadb48aaf99a31
Roel Van de Paar <roel.vandepaar@mariadb.com>
2023-05-20 14:36:44 +1000

    Bug updates

--- BUGS/MDEV-27575.sql
+++ BUGS/MDEV-27575.sql
@@ -1,3 +1,7 @@
 INSTALL PLUGIN spider SONAME 'ha_spider.so';
 SET GLOBAL default_tmp_storage_engine=spider;
 SHUTDOWN;
+
+INSTALL PLUGIN Spider SONAME 'ha_spider.so';
+SET GLOBAL default_storage_engine=Spider;
+SHUTDOWN;
--- BUGS/MDEV-31117.sql
+++ BUGS/MDEV-31117.sql
@@ -1,2 +1,5 @@
 INSTALL PLUGIN Spider SONAME 'ha_spider.so';
 CREATE TABLE t (c INT) ENGINE=Spider COMMENT='abc';
+
+INSTALL PLUGIN Spider SONAME 'ha_spider.so';
+ALTER TABLE mysql.help_topic ENGINE=Spider;
