1d27265c6c11277db475f76f2b3ec6c4aadccdc6
mariadb-RameshSivaraman <ramesh.sivaraman@mariadb.com>
2023-08-24 10:32:54 +0530

    bug updates

--- BUGS/MDEV-31813.sql
+++ BUGS/MDEV-31813.sql
@@ -1,2 +1,5 @@
 # mysqld options required for replay:  --innodb-force-recovery=6
 SET GLOBAL innodb_undo_log_truncate=1;
+
+# mysqld options required for replay:  --innodb-read-only=1
+SET GLOBAL innodb_max_purge_lag_wait=1;
