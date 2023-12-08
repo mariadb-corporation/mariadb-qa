417520a8e0cbe76090cb0d5dab5f1a6165c788b9
mariadb-RameshSivaraman <ramesh.sivaraman@mariadb.com>
2023-12-08 11:18:18 +0530

    Bug updates

--- BUGS/MDEV-25488.sql
+++ BUGS/MDEV-25488.sql
@@ -3,3 +3,10 @@ SET GLOBAL max_binlog_stmt_cache_size=0;
 CREATE TEMPORARY TABLE t (c INT) ENGINE=InnoDB;
 SELECT @@GLOBAL.innodb_flush_method=variable_value FROM information_schema.global_variables;
 DELETE FROM mysql.proc;
+
+# mysqld options required for replay: --log-bin --binlog_format=ROW
+CREATE TABLE t (a INT) ENGINE=MyISAM;
+SET GLOBAL max_binlog_stmt_cache_size=1;
+INSERT t VALUES (1),(1),(1),(1),(1),(1),(1),(1);
+INSERT INTO t SELECT 1 FROM t A,t B,t C, t D;
+ANALYZE UPDATE t SET a=10;
