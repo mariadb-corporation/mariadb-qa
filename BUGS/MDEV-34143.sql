c9ea31cf803e5ec1d48d195789d90a35008d319a
Roel Van de Paar <roel.vandepaar@mariadb.com>
2024-05-15 07:20:04 +1000

    Bug update

--- BUGS/MDEV-34143.sql
+++ BUGS/MDEV-34143.sql
@@ -6,3 +6,7 @@ SELECT JSON_EXTRACT('[["A",2]]','$[0]');
 
 SET collation_connection=ucs2_general_ci;
 SELECT JSON_EXTRACT(JSON_COMPACT ('{"abc": "foo"}'),'$.abc');
+
+SET character_set_database=ucs2;
+SET CHARACTER SET cp1251_koi8;
+SELECT JSON_EXTRACT('{ "\\"a\\"": 1}','$."\\"a\\""');
