f59bb7b25e411db66e400b6582079c2f31ab0784
mariadb-RameshSivaraman <ramesh.sivaraman@mariadb.com>
2024-05-27 15:38:01 +0530

    bug updates

--- BUGS/MDEV-34230.sql
+++ BUGS/MDEV-34230.sql
@@ -31,3 +31,17 @@ DELETE FROM t;
 INSERT INTO t VALUES (1);
 SET GLOBAL key_cache_segments=1;
 SELECT * FROM t;  # [ERROR] Got error 126 when reading table / HA_ERR_CRASHED (126): Index for table is corrupt
+
+# mysqld options required for replay:  --log-bin
+CREATE TEMPORARY TABLE t (a INT PRIMARY KEY,c CHAR,INDEX sec_index (c)) engine=MyISAM;
+INSERT INTO t VALUES (1,1),(2,2);
+SET GLOBAL key_cache_segments=1;
+SELECT * FROM t LIMIT 1;
+DELETE FROM t;
+INSERT INTO t VALUES (1,1),(2,2),(3,3),(4,4),(5,5),(6,6),(7,7),(8,8),(9,9),(10,10),(11,11),(1,1);
+DELETE FROM t;
+INSERT INTO t VALUES (1,1);
+DELETE FROM t;
+INSERT INTO t VALUES (1,1);
+SET GLOBAL key_cache_segments=1;
+SELECT * FROM t;
