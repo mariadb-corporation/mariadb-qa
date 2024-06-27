ecb8b3ddc0c067f53755f81609c6f842942393a8
mariadb-RameshSivaraman <ramesh.sivaraman@mariadb.com>
2024-06-27 15:00:59 +0530

    bug updates

--- BUGS/MDEV-31825.sql
+++ BUGS/MDEV-31825.sql
@@ -1,2 +1,5 @@
 # mysqld options required for replay:  --skip-grant-tables=1
 PREPARE s0 FROM 'SHOW GRANTS FOR unkown_user';
+
+# mysqld options required for replay:  --skip-grant-tables=1
+CREATE ROLE r1 WITH ADMIN unknown_user;
