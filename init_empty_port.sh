6b0188e1a9440eb64ccd7ffcf746328c7bb73a5d
Roel Van de Paar <roel.vandepaar@mariadb.com>
2024-07-30 15:52:11 +1000

    script updates

--- init_empty_port.sh
+++ init_empty_port.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-# Find empty port
-init_empty_port(){
+# Source this script from your script and then you can use init_empty_port() to find an empty port. 
+# The empty port number will then be stored in the ${NEWPORT} variable for you to use.
+init_empty_port(){  # Find an empty port
   # Choose a random port number in 10-13K range (**), with triple check to confirm it is free
   NEWPORT=$[ 10001 + ( ${RANDOM} % 3000 ) ]
   DOUBLE_CHECK=0
   while :; do
-    # Check if the port is free in three different ways
+    # Check if the port is free in four different ways
     ISPORTFREE1="$(netstat -an | tr '\t' ' ' | grep -E --binary-files=text "[ :]${NEWPORT} " | wc -l)"
     ISPORTFREE2="$(ps -ef | grep --binary-files=text "port=${NEWPORT}" | grep --binary-files=text -v 'grep')"
     ISPORTFREE3="$(grep --binary-files=text -o "port=${NEWPORT}" /test/*/start 2>/dev/null | wc -l)"
-    if [ "${ISPORTFREE1}" -eq 0 -a -z "${ISPORTFREE2}" -a "${ISPORTFREE3}" -eq 0 ]; then
+    ISPORTFREE4="$(netstat -tuln | grep :${NEWPORT})"
+    if [ "${ISPORTFREE1}" -eq 0 -a -z "${ISPORTFREE2}" -a "${ISPORTFREE3}" -eq 0 -a -z "${ISPORTFREE4}" ]; then
       if [ "${DOUBLE_CHECK}" -eq 2 ]; then  # If true, then the port was triple checked (to avoid races) to be free
         break  # Suitable port number found
       else
