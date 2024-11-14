6010e0db0d576329db85152f87d39a99c7655137
mariadb-RameshSivaraman <ramesh.sivaraman@mariadb.com>
2024-11-14 11:42:39 +0530

    bug updates

---
+++ BUGS/MDEV-35397.sql
@@ -0,0 +1,9 @@
+SET GLOBAL rpl_semi_sync_master_enabled=1;
+CREATE TABLE t (c INT);
+FLUSH STATUS;
+CREATE OR REPLACE TABLE t (c INT);
+START TRANSACTION;
+INSERT INTO t (c) VALUES (1);
+SET GLOBAL rpl_semi_sync_master_enabled=0;
+SET GLOBAL rpl_semi_sync_master_enabled=1;
+CALL foo();
