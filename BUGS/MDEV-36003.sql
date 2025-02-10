4956e1ea58189e3dc6af12d4abdc7ebd122f395e
Roel Van de Paar <roel.vandepaar@mariadb.com>
2025-02-10 15:19:17 +1100

    Bug updates, script updates

--- BUGS/MDEV-36003.sql
+++ BUGS/MDEV-36003.sql
@@ -1,2 +1,5 @@
 CREATE USER user@localhost;
 SELECT authentication_string <> '' FROM mysql.user;
+
+SET SESSION sql_buffer_result=ON;
+SELECT authentication_string <>''FROM mysql.user;
---
+++ BUGS/MDEV-36046.sql
@@ -0,0 +1,2 @@
+SET sql_select_limit=3;
+SELECT * FROM mysql.user ORDER BY PASSWORD;
