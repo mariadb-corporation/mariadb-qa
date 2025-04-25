bef94e93e5b25380893ada5390b267acad0ca103
Roel Van de Paar <roel.vandepaar@mariadb.com>
2025-04-25 19:47:16 +1000

    Bug updates

--- BUGS/MDEV-23956.sql
+++ BUGS/MDEV-23956.sql
@@ -18,3 +18,11 @@ SET max_statement_time=0.1;
 INSTALL PLUGIN RocksDB SONAME 'ha_rocksdb.so';
 CREATE TABLE t (a INT) ENGINE=RocksDB;
 CREATE INDEX i ON t (a);
+
+INSTALL SONAME 'ha_rocksdb';
+CREATE TABLE t (c CHAR(10) BINARY CHARACTER SET 'utf8' COLLATE 'utf8_bin',c2 CHAR(1) BINARY,c3 VARCHAR(2) BINARY CHARACTER SET 'BINARY' COLLATE 'BINARY',c4 VARCHAR(254) BINARY CHARACTER SET 'BINARY' COLLATE 'BINARY') ENGINE=RocksDB ROW_FORMAT=COMPRESSED;
+ALTER TABLE t ADD INDEX (c2,c4,c3);
+ALTER TABLE t ADD INDEX (c3,c4,c2,c);
+ALTER TABLE t ADD INDEX (c2,c3,c4);
+SET max_statement_time=0.001;
+ALTER TABLE t ADD INDEX (c4,c3,c2,c);   # Repeat till an UBSAN issue is observed. Best way to reproduce is to paste a large number of these queries into the CLI i.e. query; query; query. This will generally generate the issue after about 30-50 queries.
