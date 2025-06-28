c6ade5557eb2cb9c5d62ba08861beec9a88fc47e
Roel Van de Paar <roel.vandepaar@mariadb.com>
2025-06-28 22:34:39 +1000

    Bug updates, script updates

--- BUGS/MDEV-37097.sql
+++ BUGS/MDEV-37097.sql
@@ -10,3 +10,25 @@ INSERT INTO t SET a=1;
 SET GLOBAL innodb_limit_optimistic_insert_debug=0;
 DELETE FROM t LIMIT 2;
 INSERT INTO t VALUES (1,'a');
+
+SET sql_mode='';
+SET NAMES latin1;
+CREATE TABLE t (c CHAR KEY,INDEX(c)) ENGINE=InnoDB;
+SET GLOBAL innodb_limit_optimistic_insert_debug=2;
+INSERT INTO t VALUES ('İóëɠ'),(0),('20000');
+DELETE FROM t LIMIT 3;
+INSERT INTO t VALUES (1);
+SET GLOBAL innodb_limit_optimistic_insert_debug=0;
+DELETE FROM t LIMIT 1;
+INSERT INTO t VALUES ('a');
+
+SET sql_mode='';
+SET NAMES latin1;
+CREATE TABLE t (c CHAR KEY,INDEX(c)) ENGINE=InnoDB;
+SET GLOBAL innodb_limit_optimistic_insert_debug=1;
+INSERT INTO t VALUES ('İóëɠ'),(0),('20000');
+DELETE FROM t LIMIT 2;
+INSERT INTO t VALUES (1);
+SET GLOBAL innodb_limit_optimistic_insert_debug=0;
+DELETE FROM t LIMIT 1;
+INSERT INTO t VALUES ('a');
---
+++ audit_preload.sql
@@ -0,0 +1,4 @@
+INSTALL PLUGIN server_audit SONAME 'server_audit';
+SET GLOBAL server_audit_logging=ON;
+SET GLOBAL server_audit_sync_log_file=ON;
+SET GLOBAL server_audit_file_buffer_size=1;
