3058f0b1c6ee7cd2ae16d6bdf5bcceeaece91b75
Saahil Alam <saahil.alam@mariadb.com>
2025-07-08 00:34:14 +0530

    Bug updates

---
+++ BUGS/MDEV-37173.sql
@@ -0,0 +1,8 @@
+CREATE OR REPLACE TABLE mysql.general_log (a INT)Engine=InnoDB;
+CREATE TABLE t (c TIMESTAMP KEY)Engine=InnoDB;
+SET GLOBAL general_log=1;
+SET GLOBAL log_output='FILE,TABLE';
+SET @@autocommit=0;
+SELECT * FROM t;
+TRUNCATE t;
+ALTER TABLE t DISCARD TABLESPACE;
