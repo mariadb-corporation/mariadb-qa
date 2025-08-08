282484d9ee2cb30a12b7f9114804c6352716656d
Roel Van de Paar <roel.vandepaar@mariadb.com>
2025-08-08 16:16:09 +1000

    Build script bugfix

--- mariadb-build-qa/clone.sh
+++ mariadb-build-qa/clone.sh
@@ -4,8 +4,8 @@
 if [ -z "${1}" ]; then echo "Assert: please specify a version, like 11.2"; exit 1; fi
 if [[ "${1}" == "10."* || "${1}" == "11."* || "${1}" == "12."* ]]; then rm -Rf ${1}; fi
 
-if [ "${1}" == "12.1" ]; then
-  git clone --depth=1 --recurse-submodules -j8 https://github.com/MariaDB/server.git $1 &  # Trunk == 12.0
+if [ "${1}" == "12.2" ]; then
+  git clone --depth=1 --recurse-submodules -j8 https://github.com/MariaDB/server.git $1 &  # Trunk == 12.2
 else
   git clone --depth=1 --recurse-submodules -j8 --branch=$1 https://github.com/MariaDB/server.git $1 &
 fi
