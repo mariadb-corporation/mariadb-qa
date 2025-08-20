597d907c2351ca405000213fa28e847e19960309
mariadb-RameshSivaraman <ramesh.sivaraman@mariadb.com>
2025-08-20 13:29:31 +0530

    updated mark_as_fixed.sh script

--- mark_as_fixed.sh
+++ mark_as_fixed.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
+KB_FILE=$1
+  if [ -r $KB_FILE ]; then
+else
+  echo "$KB_FILE not found"
+  exit 1
+fi
 
 TMPRUN="$(mktemp)"
 if [ -r mark_as_fixed.list ]; then
   grep 'Fixed' mark_as_fixed.list | grep -oE 'MENT-[0-9]+|MDEV-[0-9]+' > ${TMPRUN}
   while read BUG; do
     CURLINES="$(mktemp)"
-    grep "${BUG}" known_bugs.strings | grep -v '## Fixed' > ${CURLINES}
+    grep "${BUG}" $KB_FILE | grep -v '## Fixed' > ${CURLINES}
     while read LINE; do
       LINEMOD="$(echo "${LINE}" | sed 's{         ## MDEV{## MDEV{;s{## MDEV{## Fixed ## MDEV{' | sed 's{^{# {')"  # { is not a symbol used in known bugs
-      echo "${LINEMOD}" >> known_bugs.strings  # Add line to end as ## Fixed
-      sed -i "/${LINE}/d" known_bugs.strings  # Remove the original line
+      echo "${LINEMOD}" >> $KB_FILE # Add line to end as ## Fixed
+      ESCAPED_LINE="${LINE//|/\\|}"
+      sed -i "\|${ESCAPED_LINE}|d" $KB_FILE # Remove the original line
       LINE=; LINEMOD=
     done < ${CURLINES}
     if [ ! -z "${CURLINES}" -a -r "${CURLINES}" ]; then rm -f ${CURLINES}; fi
