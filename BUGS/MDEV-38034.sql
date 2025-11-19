5b25334979436e274a68c82c6296c8438d1d893c
mariadb-RameshSivaraman <ramesh.sivaraman@mariadb.com>
2025-11-19 12:35:42 +0530

    bug updates

--- BUGS/MDEV-38034.sql
+++ BUGS/MDEV-38034.sql
@@ -6,3 +6,10 @@ SET transaction_read_only=1;
 HANDLER t2 OPEN;
 SET transaction_read_only=0;
 INSERT INTO t1 VALUES ('a');
+
+CREATE TABLE t (c1 INT) ENGINE=INNODB;
+SET transaction_read_only=1;
+SET transaction_isolation='SERIALIZABLE';
+HANDLER t OPEN;
+SET transaction_read_only=0;
+ALTER TABLE t DISCARD TABLESPACE;
