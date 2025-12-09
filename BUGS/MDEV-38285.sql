ca8165bb8f76108b635fb6f20d966afd9aff2c11
Saahil Alam <saahil.alam@mariadb.com>
2025-12-09 23:44:02 +0530

    BUG updates

---
+++ BUGS/MDEV-38285.sql
@@ -0,0 +1,5 @@
+SET GLOBAL innodb_lru_scan_depth=86400;
+SET debug_dbug='+d,page_intermittent_checksum_mismatch';
+CREATE TEMPORARY TABLE t (i INT);
+SELECT * FROM mysql.transaction_registry;
+CREATE TEMPORARY TABLE 龡龡龡 (丌丌丌 INT) DEFAULT CHARSET=utf8;
