b3a916fe4412b97e4486340c13ed21d99246f239
Saahil Alam <saahil.alam@mariadb.com>
2026-06-01 15:32:26 +0530

    Bug updates

--- BUGS/SPECIAL-55.sql
+++ BUGS/SPECIAL-55.sql
@@ -5,3 +5,6 @@ CREATE TABLE t2 (c1 INT,c2 VARCHAR(1)) ENGINE=CONNECT TABLE_TYPE=FIX FILE_NAME='
 RENAME TABLE t2 TO t2_v;
 CREATE TABLE t3 LIKE t1;
 DELETE FROM a3,a1 USING t2 AS a1 LEFT JOIN t3 AS a2 ON a1.c1=a2.c1 LEFT   JOIN t3 AS a3 ON a2.c1=a3.c1;
+# CLI: ERROR 1296 (HY000): Got error 174 'Open(r+b) error 2 on /tmp/c_84.fix: No such file or directory' from CONNECT
+# ERR: OpenTable: Open(r+b) error 2 on /tmp/c_84.fix: No such file or directory
+# ERR: [ERROR] Got error 174 when reading table './test/t2'
