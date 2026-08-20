bbcb2bbd71e547853f4fcfa64b1074e124587fdf
Saahil Alam <saahil.alam@mariadb.com>
2026-08-20 12:51:27 +0000

    Bug updates

--- BUGS/MDEV-39593.sql
+++ BUGS/MDEV-39593.sql
@@ -1,3 +1,8 @@
 SELECT EXTRACTVALUE('<a/>', '/a[number()]');
 
 SELECT EXTRACTVALUE ('<a/','/a[number()');
+
+CREATE TABLE t1 (c INT);
+LOCK TABLES t1 READ;
+GRANT SELECT ON d.* TO u;
+SELECT EXTRACTVALUE('','/a[number()]');
